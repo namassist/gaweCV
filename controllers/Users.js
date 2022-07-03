9375cfe2a58f8ea7294030e28472db39495b88d5
namassist <chairulanmm@gmail.com>
2022-07-04 01:40:33 +0700

    create register endpoint

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,9 +1,31 @@
 import Users from "../models/UserModel.js";
+import bcrypt from "bcrypt";
 
 export const getUsers = async (req, res) => {
   try {
     const users = await Users.findAll();
     res.json(users);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const register = async (req, res) => {
+  const { name, email, password, confirmPassword, role } = req.body;
+  if (password !== confirmPassword)
+    return res
+      .status(400)
+      .json({ message: "password dan konfirmasi password tidak cocok" });
+  const salt = await bcrypt.genSalt();
+  const hashPassword = await bcrypt.hash(password, salt);
+  try {
+    await Users.create({
+      name: name,
+      email: email,
+      password: hashPassword,
+      role: role,
+    });
+    res.json({ status: 200, message: "register berhasil" });
   } catch (error) {
     console.log(error);
   }
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getUsers } from "../controllers/Users.js";
+import { getUsers, register } from "../controllers/Users.js";
 
 const router = express.Router();
 
 router.get("/users", getUsers);
+router.post("/users", register);
 
 export default router;

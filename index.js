23f743064c9a25ed2cc97500b0bc01f7d121b96a
namassist <chairulanmm@gmail.com>
2022-07-04 01:27:18 +0700

    create and define router in main file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "./config/Database.js";
+import router from "./routes/index.js";
 const app = express();
 
 try {
@@ -8,6 +9,9 @@ try {
 } catch (error) {
   console.log(error);
 }
+
+app.use(express.json());
+app.use(router);
 
 app.listen(5000, () => {
   console.log("Server berjalan pada port 5000");
